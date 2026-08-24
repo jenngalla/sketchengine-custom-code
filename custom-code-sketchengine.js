a7bea406ad805d010126c96febcc1ad1194a738a
jenngalla <jenngalla@hotmail.com>
2026-08-24 21:27:14 +0100

    Replace Tippy.js help icon with alert message
    Removed Tippy.js help icon implementation and added an alert.

--- custom-code-sketchengine.js
+++ custom-code-sketchengine.js
@@ -1,18 +1,1 @@
-V2 TIPPY
-  <!-- Tippy.js libraries -->
-<script src="https://unpkg.com/@@2
-pkg.com/tippy.js@6"></script>
-
-<!-- Example help icon -->
-<span id="help-upload" style="cursor:pointer;">❓</span>
-
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-  tippy('#help-upload', {
-    content: 'Upload your data file here to begin.',
-    trigger: 'click',
-    interactive: true,
-    placement: 'right'
-  });
-});
-</script>
+alert("Custom JavaScript is running");
