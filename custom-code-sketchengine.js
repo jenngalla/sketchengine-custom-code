da1840a50c5b7b187d5eb5fbbae1f9fc389ca18d
jenngalla <jenngalla@hotmail.com>
2026-08-24 21:20:26 +0100

    Simplify help icons and tooltips
    Removed multiple help icons and their tooltips, retaining only the upload help icon.

--- custom-code-sketchengine.js
+++ custom-code-sketchengine.js
@@ -1,35 +1,17 @@
-<!-- Tippy.js -->
-https://unpkg.com/@popperjs/core@2>
-https://unpkg.com/tippy.js@6script>
+<!-- Tippy.js libraries -->
+<script src="https://unpkg.com/@@2
+pkg.com/tippy.js@6"></script>
 
-<!-- Example help icons -->
+<!-- Example help icon -->
 <span id="help-upload" style="cursor:pointer;">❓</span>
-<span id="help-search" style="cursor:pointer;">❓</span>
-<span id="help-results" style="cursor:pointer;">❓</span>
 
 <script>
 document.addEventListener('DOMContentLoaded', function () {
-
   tippy('#help-upload', {
     content: 'Upload your data file here to begin.',
     trigger: 'click',
     interactive: true,
     placement: 'right'
   });
-
-  tippy('#help-search', {
-    content: 'Search for records using keywords or filters.',
-    trigger: 'click',
-    interactive: true,
-    placement: 'right'
-  });
-
-  tippy('#help-results', {
-    content: 'Your results will appear in this section after processing.',
-    trigger: 'click',
-    interactive: true,
-    placement: 'right'
-  });
-
 });
 </script>
